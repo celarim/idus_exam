2c9ef3cc9e1952e6f903e31b7d2fb736bcd004a2
celarim <k6jun@kookmin.ac.kr>
2025-02-11 16:01:35 +0900

    Fix logout
    불필요 객체 삭제

--- src/main/java/com/example/idus_exam/config/SecurityConfig.java
+++ src/main/java/com/example/idus_exam/config/SecurityConfig.java
@@ -6,12 +6,9 @@ import com.example.idus_exam.config.filter.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.saml2.Saml2LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,8 +33,6 @@ public class SecurityConfig {
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
         http.sessionManagement(AbstractHttpConfigurer::disable);
-
-        HeaderWriterLogoutHandler clearSiteData = new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.COOKIES));
 
         http.authorizeHttpRequests(
                 (auth) ->auth
--- src/main/java/com/example/idus_exam/config/filter/JwtLogoutHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.idus_exam.config.filter;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-
-
