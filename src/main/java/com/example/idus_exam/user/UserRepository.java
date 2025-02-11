60712231ba15d71e730e4ba9c705fe07ecf2dac7
celarim <k6jun@kookmin.ac.kr>
2025-02-11 14:52:15 +0900

    Feat User 목록 구현, email과 name기반으로 검색 구현
    모든 함수가 page와 size를 받아 페이지네이션 적용
    /user/list로 접근
    유저 상세와 유저 최근 주문을 전달
    주문이 없다면 null 값으로 전달
    /user/list/email
    email을 기반으로
    유저 상세와 유저 최근 주문을 전달
    주문이 없다면 null 값으로 전달
    user/list/name
    name을 기반으로
    유저 상세와 유저 최근 주문을 전달
    주문이 없다면 null 값으로 전달

--- src/main/java/com/example/idus_exam/user/UserRepository.java
+++ src/main/java/com/example/idus_exam/user/UserRepository.java
@@ -1,10 +1,15 @@
 package com.example.idus_exam.user;
 
 import com.example.idus_exam.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Page<User> findAllByEmailContaining(Pageable pageable, String email);
+    Page<User> findAllByNameContaining(Pageable pageable, String name);
 }
--- src/main/java/com/example/idus_exam/user/UserService.java
+++ src/main/java/com/example/idus_exam/user/UserService.java
@@ -5,6 +5,8 @@ import com.example.idus_exam.user.model.User;
 import com.example.idus_exam.user.model.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +26,7 @@ public class UserService implements UserDetailsService {
         return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 
+    //회원 가입
     public ResponseEntity<String> signup(UserDto.SignupRequest dto) {
         User user = dto.toEntity();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -31,14 +34,34 @@ public class UserService implements UserDetailsService {
         return ResponseEntity.ok("Signup successful");
     }
 
+    //유저 상세 정보
     public ResponseEntity<UserDto.DetailResponse> getUserDetail(User user) {
         User result = userRepository.findById(user.getIdx()).orElseThrow();
         return ResponseEntity.ok(UserDto.DetailResponse.from(result));
     }
 
+    //유저 주문 목록
     public ResponseEntity<List<UserDto.OrdersResponse>> getUserOrders(User user) {
         User result = userRepository.findById(user.getIdx()).orElseThrow();
         List<Orders> ordersList = result.getOrdersList();
         return ResponseEntity.ok(UserDto.OrdersResponse.from(ordersList));
+    }
+
+    //유저 목록
+    public ResponseEntity<List<UserDto.UserListResponse>> getUserList(int page, int size) {
+        Page<User> users = userRepository.findAll(PageRequest.of(page, size));
+        return ResponseEntity.ok(UserDto.UserListResponse.from(users.getContent()));
+    }
+
+    //유저 목록 이메일로
+    public ResponseEntity<List<UserDto.UserListResponse>> getUserListByEmail(int  page, int size, String email) {
+        Page<User> users = userRepository.findAllByEmailContaining(PageRequest.of(page, size), email);
+        return ResponseEntity.ok(UserDto.UserListResponse.from(users.getContent()));
+    }
+
+    //유저 목록 이름으로
+    public ResponseEntity<List<UserDto.UserListResponse>> getUserListByName(int  page, int size, String name) {
+        Page<User> users = userRepository.findAllByNameContaining(PageRequest.of(page, size), name);
+        return ResponseEntity.ok(UserDto.UserListResponse.from(users.getContent()));
     }
 }
